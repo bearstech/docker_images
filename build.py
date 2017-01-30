ac1bf663bf35bb3e8fdf86900259a347a34b5e68
Gael Pasgrimaud <gael@gawel.org>
2017-01-30 15:58:30 +0100

    builder

--- build.py
+++ build.py
@@ -1,37 +1,43 @@
 import subprocess
+import sys
 
-TEMPLATE = '''
+DEBIAN = '''
 from {os}:{version}
 
 RUN apt-get update && apt-get -y dist-upgrade && apt-get install -y python{py}
 '''
 
-TESTING = r'''
+DEBIAN_TESTING = r'''
 RUN apt-get install -y build-essential python{py}-dev python-virtualenv
 RUN mkdir -p /tmp/nuka_provisionning/nuka && \
     virtualenv -p python{py} /tmp/nuka_provisionning/nuka && \
     /tmp/nuka_provisionning/nuka/bin/pip install -U pip coverage
 '''
 
+all_branches = []
+
 
 def gen_docker_file(os, version, py, testing=False):
     branch = '%s-%s-python%s' % (os, version, py)
     if py == '2':
         py = '2.7'
-    dockerfile = TEMPLATE.format(os=os, version=version, py=py)
+    dockerfile = DEBIAN.format(os=os, version=version, py=py)
     if testing:
         branch += '-testing'
-        dockerfile += TESTING.format(py=py)
+        dockerfile += DEBIAN_TESTING.format(py=py)
 
     print(branch)
     print('==========================================')
     print(dockerfile)
     subprocess.call(['git', 'branch', branch])
     subprocess.check_call(['git', 'co', branch])
+    all_branches.append(branch)
     with open('Dockerfile', 'w') as fd:
         fd.write(dockerfile)
     subprocess.check_call(['git', 'add', 'Dockerfile'])
     subprocess.call(['git', 'ci', '-m', 'update'])
+    if '--push' in sys.argv:
+        subprocess.call(['git', 'push', 'origin', branch])
 
 
 debians = (
@@ -59,6 +65,8 @@ def main():
             fd.write(dockerfile)
         subprocess.check_call(['git', 'add', 'Dockerfile'])
         subprocess.check_call(['git', 'ci', '-m', 'update'])
+        if '--push' in sys.argv:
+            subprocess.call(['git', 'push', 'origin', 'master'])
 
 
 if __name__ == '__main__':
