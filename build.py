5625e656e9f70e0f5b38214be3641f5fdca937b5
Gael Pasgrimaud <gael@gawel.org>
2017-01-30 20:37:26 +0100

    builder

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ RUN apt-get update && apt-get -y dist-upgrade && \
     apt-get install -y python{py} && \
     apt-get clean
 
-CMD ['bash', '-c', 'while true; do sleep 9999999999999999999; done']
+CMD ["/bin/bash", "-c", "while true; do sleep 99999999999999; done"]
 '''
 
 DEBIAN_TESTING = r'''
@@ -21,7 +21,7 @@ RUN mkdir -p /tmp/nuka_provisionning/nuka && \
     virtualenv -p python{py} /tmp/nuka_provisionning/nuka && \
     /tmp/nuka_provisionning/nuka/bin/pip install -U pip coverage
 
-CMD ['bash', '-c', 'while true; do sleep 9999999999999999999; done']
+CMD ["/bin/bash", "-c", "while true; do sleep 99999999999999; done"]
 '''
 
 all_branches = []
