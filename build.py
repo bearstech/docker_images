c66259e025e9957f84147a1ccd6068af006f6f0e
Gael Pasgrimaud <gael@gawel.org>
2017-05-04 10:45:40 +0200

    set locale

--- build.py
+++ build.py
@@ -14,6 +14,10 @@ RUN apt-get update && apt-get -y dist-upgrade && \
     apt-get install -y python{py} ca-certificates adduser curl gnupg && \
     apt-get clean && rm -rf /var/lib/apt/lists/*
 
+RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen \
+    echo 'LANG="en_US.UTF-8"'>/etc/default/locale \
+    dpkg-reconfigure --frontend=noninteractive locales
+
 CMD ["/bin/bash", "-c", "while true; do sleep {sleep}; done"]
 ''',
     debian_testing=r'''
