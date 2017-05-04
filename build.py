7b2e649c048ed44676e6db211ec03120de679c1e
Gael Pasgrimaud <gael@gawel.org>
2017-05-04 22:01:24 +0200

    lighter

--- build.py
+++ build.py
@@ -5,10 +5,35 @@ import sys
 
 sleep = str(int(time.time()))
 
+DEBIAN_CLEAN = r'''
+RUN apt-get clean && \
+    rm -rf /root/.cache && \
+    rm -rf /tmp/* /var/tmp/* && \
+    rm -rf /etc/dpkg/dpkg.cfg.d/02apt-speedup && \
+    rm -rf /var/cache/debconf/*-old && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /usr/share/doc/* && \
+    rm -rf /usr/share/man/* /usr/share/groff/* /usr/share/info/* && \
+    rm -rf /usr/share/lintian/* /usr/share/linda/* /var/cache/man/*
+'''
+
+DEBIAN_NODOC = r'''
+path-exclude /usr/share/doc/*
+# we need to keep copyright files for legal reasons
+path-include /usr/share/doc/*/copyright
+path-exclude /usr/share/man/*
+path-exclude /usr/share/groff/*
+path-exclude /usr/share/info/*
+# lintian stuff is small, but really unnecessary
+path-exclude /usr/share/lintian/*
+path-exclude /usr/share/linda/*
+'''
 
 TEMPLATES = dict(
     debian=r'''
 from {image}
+
+COPY 01_nodoc /etc/dpkg/dpkg.cfg.d/01_nodoc
 
 RUN apt-get update && apt-get -y dist-upgrade && \
     apt-get install -y --no-install-recommends \
@@ -18,13 +43,7 @@ RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     echo 'LANG="en_US.UTF-8"'>/etc/default/locale && \
     dpkg-reconfigure --frontend=noninteractive locales
 
-RUN apt-get clean && \
-    rm -rf /tmp/* /var/tmp/* && \
-    rm -rf /etc/dpkg/dpkg.cfg.d/02apt-speedup && \
-    rm -rf /usr/share/locale/* && \
-    rm -rf /var/cache/debconf/*-old && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm -rf /usr/share/doc/*
+{clean}
 
 CMD ["/bin/bash", "-c", "while true; do sleep {sleep}; done"]
 ''',
@@ -38,13 +57,7 @@ RUN mkdir -p /tmp/nuka_provisionning/nuka && \
     virtualenv -p python{py} /tmp/nuka_provisionning/nuka && \
     /tmp/nuka_provisionning/nuka/bin/pip install -U pip coverage
 
-RUN apt-get clean && \
-    rm -rf /tmp/* /var/tmp/* && \
-    rm -rf /etc/dpkg/dpkg.cfg.d/02apt-speedup && \
-    rm -rf /usr/share/locale/* && \
-    rm -rf /var/cache/debconf/*-old && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm -rf /usr/share/doc/*
+{clean}
 
 CMD ["/bin/bash", "-c", "while true; do sleep {sleep}; done"]
 ''',
@@ -80,10 +93,11 @@ def gen_docker_file(os, version, py, testing=False):
         image = 'bearstech/nukai:' + branch
         branch += '-testing'
         dockerfile = TEMPLATES[os + '_testing'].format(
-            image=image, py=py, sleep=sleep)
+            image=image, py=py, clean=DEBIAN_CLEAN, sleep=sleep)
     else:
         image = '{os}:{version}'.format(os=os, version=version)
-        dockerfile = TEMPLATES[os].format(image=image, py=py, sleep=sleep)
+        dockerfile = TEMPLATES[os].format(
+            image=image, py=py, clean=DEBIAN_CLEAN, sleep=sleep)
 
     print(branch)
     print('==========================================')
@@ -93,6 +107,9 @@ def gen_docker_file(os, version, py, testing=False):
     all_branches.append(branch)
     with open('Dockerfile', 'w') as fd:
         fd.write(dockerfile)
+    if branch.startswith('debian'):
+        with open('01_nodoc', 'w') as fd:
+            fd.write(DEBIAN_NODOC)
     subprocess.call(['rm', '-f', 'build.py'])
     subprocess.check_call(['git', 'add', '-A'])
     subprocess.call(['git', 'commit', '-m', 'update'])
