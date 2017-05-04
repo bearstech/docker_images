a0a9afd49385b29264158989243cda38be47647f
Gael Pasgrimaud <gael@gawel.org>
2017-05-04 20:27:45 +0200

    install locales

--- build.py
+++ build.py
@@ -10,13 +10,14 @@ TEMPLATES = dict(
     debian=r'''
 from {image}
 
-RUN apt-get update && apt-get -y dist-upgrade && \
-    apt-get install -y python{py} ca-certificates adduser curl gnupg && \
-    apt-get clean && rm -rf /var/lib/apt/lists/*
-
 RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     echo 'LANG="en_US.UTF-8"'>/etc/default/locale && \
     dpkg-reconfigure --frontend=noninteractive locales
+
+RUN apt-get update && apt-get -y dist-upgrade && \
+    apt-get install -y \
+        python{py} locales ca-certificates adduser curl gnupg && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
 
 CMD ["/bin/bash", "-c", "while true; do sleep {sleep}; done"]
 ''',
