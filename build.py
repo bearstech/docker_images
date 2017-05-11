3e2f038771bc30646f64cfffe958486a4b2cc2c9
Gael Pasgrimaud <gael@gawel.org>
2017-05-11 12:17:25 +0200

    comment

--- build.py
+++ build.py
@@ -40,6 +40,8 @@ EOF
 apt-get update && apt-get -y dist-upgrade
 apt-get install -y --no-install-recommends \
     python$1 locales ca-certificates adduser curl gnupg
+
+# set a clean locale
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 dpkg-reconfigure --frontend=noninteractive locales
