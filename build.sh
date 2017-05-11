39b91d5ed805aedc2548255afcf978c6590dc860
Gael Pasgrimaud <gael@gawel.org>
2017-05-11 12:04:14 +0200

    update

--- build.sh
+++ build.sh
@@ -36,3 +36,8 @@ apt-get install -y --no-install-recommends \
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 dpkg-reconfigure --frontend=noninteractive locales
+
+# make service start on apt install
+echo "exit 0"> /usr/sbin/policy-rc.d
+echo "echo N 3" > /sbin/runlevel
+chmod +x /sbin/runlevel
